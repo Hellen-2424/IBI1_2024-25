450df939d75fcd95bc71afe28ad4cd550db83e45
Hellen <wanling.24@intl.zju.edu.cn>
2025-03-14 04:15:27 +0100

    4

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -4,5 +4,5 @@ a=1
 for i in range(2,11):
     print(a)
     a+=i
-#print the lost last number
+#print the lost last number(since when i==10, the for loop will break and would not print the last number)
 print(a)
