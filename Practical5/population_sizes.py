7853dfed503d7bbfaa04007b9665093578d5daf2
Hellen <wanling.24@intl.zju.edu.cn>
2025-03-19 06:59:42 +0100

    5

--- Practical5/population_sizes.py
+++ Practical5/population_sizes.py
@@ -5,16 +5,21 @@ uk_countries=[57.11,3.13,1.91,5.45]#list the population
 uk_labels="England","Wales","Northern Ireland","Scotland"#Match population with country
 plt.subplot(1,2,1)
 plt.title("uk-population")
-plt.pie(uk_countries,explode=(0,0,0,0),labels=uk_labels,autopct="%1.1f%%",shadow=False,startangle=0)#draw
+plt.pie(uk_countries,explode=(0,0,0,0),labels=uk_labels,autopct="%1.1f%%",shadow=False,startangle=0,textprops={'fontsize': 6})#draw and define the size!!!#colors=
 zj_province=[65.77,41.88,45.28,61.27,85.15]#list the population
 zj_labels="Zhejiang","Fujian","Jiangxi","Anhui","Jiangsu"#Match population with the province
 plt.subplot(1,2,2)
 plt.title("zhejiang-population")
 plt.pie(zj_province,explode=(0,0,0,0,0),labels=zj_labels,autopct="%1.1f%%",shadow=False,startangle=0)
-plt.show()
+#plt.axis('equal'):make the pie be a circle not an oval
+plt.tight_layout()#auto make the picture beautiful
+
+#or use:plt.subplots_adjust(wspace=0.5, hspace=0.3)#0.5:中间间隔为子图宽度的0.5倍
 uk_countries1=sorted(uk_countries)#sort 
 zj_province1=sorted(zj_province)
-print(uk_countries1,zj_province1)
+print("uk_sorted population:",uk_countries1,"zj_sorted polulation:",zj_province1)
+plt.show()
+
 
 #or use: fig,(ax1,ax2)=plt.subplots(1,2)
 #set the size of the figure:  fig.set_size_inches(10,5)
--- Practical5/programming_language_popularity.py
+++ Practical5/programming_language_popularity.py
@@ -1,5 +1,5 @@
 #import sys #确保 Matplotlib 所在的路径在其中
-#sys.path.append('C:/Users/Hellen/AppData/Local/Microsoft/WindowsApps/python3.11.exe')
+#sys.path.append('C:/Users/Hellen/AppData/Local/Microsoft/WindowsApps/python3.11.exe')#deleted it because found the globle python path
 import matplotlib.pyplot as plt
 dic={"JavaScript":62.3,"HTML":52.9,"Python":51,"SQL":51,"TypeScript":38.5}#make and match the dictionary
 n="HTML"#create the valuable which should have been inputed (highlight it)
@@ -9,6 +9,7 @@ plt.xlabel("language")#define the label
 plt.ylabel("Usrs(percentage)")
 plt.title("programming language popularity")#make sure the title
 plt.bar(dic.keys(),dic.values())#draw
+
 plt.show()
 
 
