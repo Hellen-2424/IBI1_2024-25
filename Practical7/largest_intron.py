2346e33f9852d8c0141de704284719e32815c9cd
Hellen <wanling.24@intl.zju.edu.cn>
2025-04-02 06:21:31 +0100

    7

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -1,9 +1,9 @@
 import re
 seq='ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA' 
 #find=re.findall(r'GT+(\S)+AG',seq)改成：
-find=re.findall(r'GT(.*?)AG',seq)
+find=re.findall(r'GT.*?AG',seq)#find=re.findall(r'GT.*?AG',seq) is not the one to find the longest!
 count=0
 for i in range(len(find)):
     if len(find[i])>count:
         count=i
-print(find[count])
+print(find,find[count],len(find[count]))
