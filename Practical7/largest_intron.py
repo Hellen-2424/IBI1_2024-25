4d58cd56d3128e98a49b3891b79ace259f94f1ad
Hellen <wanling.24@intl.zju.edu.cn>
2025-04-02 07:21:25 +0100

    7

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -1,11 +1,11 @@
 import re
 seq='ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAAGTAATTTGTATGAG' 
 #find=re.findall(r'GT+(\S)+AG',seq)改成：
-find=re.findall(r'GT.*AG',seq)#find=re.findall(r'GT.*?AG',seq) is not the one to find the longest!
+find=re.findall(r'GT.*AG',seq)# it requires the greedy#find=re.findall(r'GT.*?AG',seq) is not the one to find the longest!
 count=0#initial and storage the place of longest string
-#find the longest intron
+#find the longest intron(if to find the not greedy GT...AG)
 for i in range(len(find)):
     if len(find[i])>len(find[count]):
         count=i#replace if it is longer(tips:it only contains the first)
-#or can use the function:max(最大值函数)
+#or can use the function:max(最大值函数) or use dictionary
 print(find,find[count],len(find[count]))
